17624a0ab195a3ff072252ddcd9bfe7e77eed0a6
saintsantos <saintsantos1341@live.com>
2016-01-08 13:56:29 -0500

    First commit

---
+++ app/src/main/java/com/argyetechnology/edwin/argylelauncher/MainActivity.java
@@ -0,0 +1,70 @@
+package com.argyetechnology.edwin.argylelauncher;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String APP_PACKAGE = "com.argyletechnologygroup.REDAR";
+    private Button launchBtn;
+    private Button updateBtn;
+    private String url = "http://d-h.st/d9fz"; //Need to figure out how to download app
+    private long myDownloadReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        int  uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        launchBtn = (Button) findViewById(R.id.Launch);
+        launchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launch(v);
+            }
+        });
+
+        updateBtn = (Button) findViewById(R.id.updateButton);
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                download();
+            }
+        });
+    }
+
+    public void launch(View view) {
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage(APP_PACKAGE);
+        startActivity(launchIntent);
+    }
+
+    public void download() {
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setDescription("I am downloading a logo").setTitle("Downloading logo");
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "logo.png");
+        request.setVisibleInDownloadsUi(true);
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        myDownloadReference = manager.enqueue(request);
+        //install();
+    }
+
+    public void install() {
+        Intent promptInstall = new Intent(Intent.ACTION_VIEW);
+        promptInstall.setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/Download/" + "quicklaunch.apk")), "application/vnd.android.package-archive");
+        promptInstall.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(promptInstall);
+
+    }
+}
