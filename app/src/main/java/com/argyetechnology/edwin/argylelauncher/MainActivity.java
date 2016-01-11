8534eb6f1d6811e22a05087c53388dc9fc0b1fd9
saintsantos <saintsantos1341@live.com>
2016-01-11 11:47:35 -0500

    Added TODOs and comments into the code

--- app/src/main/java/com/argyetechnology/edwin/argylelauncher/MainActivity.java
+++ app/src/main/java/com/argyetechnology/edwin/argylelauncher/MainActivity.java
@@ -27,19 +27,23 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Argyle Launcher";
     private static final String APP_PACKAGE = "com.argyletechnologygroup.REDAR";
+    private DownloadManager manager;
+    private String link = "https://www.dropbox.com/s/5tsbcvjyy2ozpfi/app.zip?dl=1";
+    private long myDownloadReference;
+    private String downloadCompleteIntentName = DownloadManager.ACTION_DOWNLOAD_COMPLETE;
+    private IntentFilter downloadCompleteIntentFilter = new IntentFilter(downloadCompleteIntentName);
+
+    //The buttons to handle the actions in the platform
     private Button launchBtn;
     private Button updateBtn;
     private Button installBtn;
-    private DownloadManager manager;
-    private String downloadCompleteIntentName = DownloadManager.ACTION_DOWNLOAD_COMPLETE;
-    private IntentFilter downloadCompleteIntentFilter = new IntentFilter(downloadCompleteIntentName);
-    private String link = "https://www.dropbox.com/s/5tsbcvjyy2ozpfi/app.zip?dl=1";
-    private long myDownloadReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //TODO: Move the update button to a menu option instead of simply a button
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Makes the app fullscreen
         View decorView = getWindow().getDecorView();
         int  uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
@@ -47,30 +51,37 @@ public class MainActivity extends AppCompatActivity {
          BroadcastReceiver downloadCompleteReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
+
                 String action = intent.getAction();
+
                 if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
-                    Log.d(TAG, "Download Complete!");
+
                     long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0);
                     DownloadManager.Query query = new DownloadManager.Query();
                     query.setFilterById(myDownloadReference);
                     Cursor c = manager.query(query);
+
                     if (downloadId != downloadId) {
                         Log.v(TAG, "Ignoring unrelated download" + downloadId);
                         return;
 
                     }
+
                     if (!c.moveToFirst()) {
                         Log.e(TAG, "Empty Row");
                         return;
                     }
 
                     int statusIndex = c.getColumnIndex(DownloadManager.COLUMN_STATUS);
+
                     if (DownloadManager.STATUS_SUCCESSFUL != c.getInt(statusIndex)) {
                         Log.w(TAG, "Download Failed");
                         return;
                     }
+
+                    Log.d(TAG, "Download Complete!");
                     install();
-                    //Log.d(TAG, "Download Complete");
+
                 }
             }
         };
@@ -102,21 +113,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launch(View view) {
+        //TODO: Make the launch button larger
+        Log.d(TAG, "Launching package");
         Intent launchIntent = getPackageManager().getLaunchIntentForPackage(APP_PACKAGE);
         startActivity(launchIntent);
     }
 
     public void download() {
-        Log.d(TAG, "Deleting files");
+        //TODO: Add check for the existence of the downloaded files before simply deleting them.
+        //Deletes files from the Download directory before downloading the updates
         String root = Environment.getExternalStorageDirectory().toString();
         File file1 = new File(root + "/Download/app.zip");
         File file2 = new File(root + "/Download/quicklaunch.apk");
+        Log.d(TAG, "Deleting files");
         file1.delete();
         file2.delete();
 
+        //Perform the required downloads for the update
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(link));
-        //request.setMimeType("application/vnd.android.package-archive");
-        request.setDescription("I am downloading a launcher").setTitle("Downloading launcher");
+        request.setDescription("I am downloading the update").setTitle("Downloading update");
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "app.zip");
         request.setVisibleInDownloadsUi(true);
         manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
@@ -125,15 +140,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void install() {
+        //Installs the application
+        Log.d(TAG, "Install application");
         unpackZip("/sdcard/Download/", "app.zip");
         Intent promptInstall = new Intent(Intent.ACTION_VIEW);
         promptInstall.setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/Download/" + "quicklaunch.apk")), "application/vnd.android.package-archive");
         promptInstall.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(promptInstall);
-
-
-
-
     }
 
     private boolean unpackZip(String path, String zipname)
@@ -143,6 +156,7 @@ public class MainActivity extends AppCompatActivity {
         try
         {
             String filename;
+            Log.d(TAG, "File path: " + path + zipname);
             is = new FileInputStream(path + zipname);
             zis = new ZipInputStream(new BufferedInputStream(is));
             ZipEntry ze;
@@ -171,16 +185,16 @@ public class MainActivity extends AppCompatActivity {
             }
 
             zis.close();
-        }
-        catch(IOException e)
-        {
+
+        } catch (IOException e) {
+
             e.printStackTrace();
             return false;
+
         }
 
         return true;
+
     }
-
-
 
 }
